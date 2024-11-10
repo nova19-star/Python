ad375348691fba00abb50710f850eecf743db8bc
nova19-star <tushar20212005@gmail.com>
2024-11-10 23:48:46 +0530

    Updated

---
+++ Data Types/tuple.py
@@ -0,0 +1,36 @@
+# Tuple      # It is a Immutable data structure that can store a collection of items of different type 
+
+tp1= (1,2,3)
+# To count perticular Element 
+tp2 = (1,7,1,7,7,1)
+
+print(tp2.count(7))     
+
+'''     # To copy a tuple to another    ---------
+tp3 = (1,2,3,9)                 ---------
+tp3.copy()                      ---------
+print(tp3)                      ---------   '''
+
+# To convert a tuple into a list 
+tp4 = (1,2,3,"hello")
+tp4 = list(tp1)
+print(tp4)
+
+# To find lenght of tuple 
+tp5 = (9,8,7,4,3,1,8)
+tp5 =  len(tp5)
+print(tp5)
+
+# To find maximum value of tuple 
+tp6 = (1,2,3,4)
+n1 = max(tp6)
+print(n1)
+
+# To find minimum value of tuple 
+tp6 = (1,2,3,4)
+n1 = min(tp6)
+print(n1)
+
+# To reverse the order of elements 
+tp7 = (1,2,3,4)
+n2 = ( )
---
+++ Operators/list.py
@@ -0,0 +1,10 @@
+# List Data type  (mutable data type used to store a collection of items of different type)
+
+lst = [1,2,3,"fact","10"]
+
+# append()      (to add item)
+lst.append(12)
+print(lst)
+
+# extend()    (to add another list to existing list)
+
